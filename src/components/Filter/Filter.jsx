f802c7367ec243dbe1a214e096c54c7df13255a8
beligrous <beligrous@gmail.com>
2023-01-24 23:17:05 +0200

    fix filter prop

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input } from '../ContactForm/contact-form.styled';
 
-function Filter({ filter, onChange }) {
+function Filter({ value, onChange }) {
   return (
     <label>
       Find contacts by name:
-      <Input type="text" value={filter} onChange={onChange} />
+      <Input type="text" value={value} onChange={onChange} />
     </label>
   );
 }
